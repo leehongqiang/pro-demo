9e0d780f8e711aa54a82e82367ca16e86bea258e
lee <407227813.qq.com>
2016-12-29 16:54:40 +0800

    更新部分设置

--- gdMap/js/map.js
+++ gdMap/js/map.js
@@ -102,7 +102,8 @@ class Maps{
       let info = [];
       AMap.service(["AMap.PlaceSearch"], function() {
         let placeSearch = new AMap.PlaceSearch({
-          type:"汽车服务|汽车销售|汽车维修|摩托车服务|餐饮服务|购物服务|生活服务|体育休闲服务|医疗保健服务|住宿服务|风景名胜|商务住宅|政府机构及社会团体|科教文化服务|交通设施服务|金融保险服务|公司企业|道路附属设施|地名地址信息|公共设施"
+          type:"汽车服务|汽车销售|汽车维修|摩托车服务|餐饮服务|购物服务|生活服务|体育休闲服务|医疗保健服务|住宿服务|风景名胜|商务住宅|政府机构及社会团体|科教文化服务|交通设施服务|金融保险服务|公司企业|道路附属设施|地名地址信息|公共设施",
+          pageSize:searchInfo.size || searchInfo.size===0?searchInfo.size:10
         });
         //判断是否用经纬度搜索
         if(searchInfo.isGeographicis){
